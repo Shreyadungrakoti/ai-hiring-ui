cba260d07a6e0d92fb452dd714a6cdd509cce76d
Shreya Dungrakoti <siaya@192.168.1.2>
2026-01-31 16:27:56 +0530

    Make logo bold curvy A and shift right

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,21 +7,28 @@ export default function Logo({ size = 44 }) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Fancy Letter A */}
+      {/* Bold Curvy Letter A */}
       <path
-        d="M 50 20 L 70 75 L 62 75 L 57 62 L 43 62 L 38 75 L 30 75 L 50 20 Z"
+        d="M 50 15 
+           C 52 15, 54 17, 55 19
+           L 75 70
+           C 76 73, 75 76, 72 77
+           L 65 77
+           C 63 77, 61 76, 60 74
+           L 57 64
+           C 56.5 62.5, 55 61, 53 61
+           L 47 61
+           C 45 61, 43.5 62.5, 43 64
+           L 40 74
+           C 39 76, 37 77, 35 77
+           L 28 77
+           C 25 76, 24 73, 25 70
+           L 45 19
+           C 46 17, 48 15, 50 15 Z"
         fill="white"
       />
-      {/* Crossbar */}
-      <rect x="44" y="55" width="12" height="3" fill="#0d1117" />
-      {/* Decorative serif top */}
-      <path
-        d="M 48 22 L 50 20 L 52 22"
-        stroke="white"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        fill="none"
-      />
+      {/* Curved cutout for crossbar */}
+      <ellipse cx="50" cy="56" rx="5" ry="3" fill="#000000" />
     </svg>
   );
 }
