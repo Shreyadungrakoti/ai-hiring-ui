1f1501dc0a13fe2879edcb56358f72e209b21662
Shreya Dungrakoti <siaya@192.168.1.2>
2026-02-03 00:00:34 +0530

    Fix logo design for proper 4 shape

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,52 +3,44 @@ export default function Logo({ size = 44 }) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 100 100"
+      viewBox="0 0 60 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       {/* Bold Curvy Number 4 */}
-      {/* Vertical right stroke */}
       <path
-        d="M 62 15
-           C 65 15, 68 18, 68 21
-           L 68 79
-           C 68 82, 65 85, 62 85
-           L 55 85
-           C 52 85, 49 82, 49 79
-           L 49 21
-           C 49 18, 52 15, 55 15
-           L 62 15 Z"
+        d="M 38 5
+           C 41 5, 43 7, 43 10
+           L 43 70
+           C 43 73, 41 75, 38 75
+           L 33 75
+           C 30 75, 28 73, 28 70
+           L 28 48
+           L 8 48
+           C 6 48, 5 47, 5 45
+           L 5 41
+           C 5 39.5, 5.5 38.5, 7 37
+           L 28 8
+           C 29 7, 30.5 5, 33 5
+           L 38 5 Z
+           M 28 35
+           L 28 19
+           L 17 35
+           L 28 35 Z"
         fill="white"
       />
       
-      {/* Diagonal top stroke */}
+      {/* Horizontal crossbar extension */}
       <path
-        d="M 30 15
-           C 33 15, 35 17, 36 20
-           L 36 23
-           C 36 25, 37 26, 39 27
+        d="M 8 41
+           C 5 41, 3 43, 3 46
+           L 3 48
+           C 3 51, 5 53, 8 53
+           L 50 53
+           C 53 53, 55 51, 55 48
            L 55 46
-           C 56 47, 55 49, 53 49
-           L 49 49
-           L 49 21
-           C 49 19, 48 18, 46 17
-           L 34 17
-           C 32 17, 30 17, 30 15 Z"
-        fill="white"
-      />
-      
-      {/* Horizontal middle stroke */}
-      <path
-        d="M 25 46
-           C 22 46, 20 48, 20 51
-           L 20 57
-           C 20 60, 22 62, 25 62
-           L 75 62
-           C 78 62, 80 60, 80 57
-           L 80 51
-           C 80 48, 78 46, 75 46
-           L 25 46 Z"
+           C 55 43, 53 41, 50 41
+           L 8 41 Z"
         fill="white"
       />
     </svg>
